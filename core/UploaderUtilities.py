8f4f1ce1a160a00e0bf01233c7a59b5059456f28
Alessio Paoletti <a.paoletti@rbw-cgi.it>
2017-03-30 13:05:12 +0200

    add groups selection and fills functions

--- core/UploaderUtilities.py
+++ core/UploaderUtilities.py
@@ -1,27 +1,33 @@
 __author__ = 'a.paoletti'
 
-import sqlite3
-
-conn = sqlite3.connect("example.db")
-
-textures_dict = (
-    "normal",
-    "diffuse",
-    "roughness",
-    "specular",
-    "occlusion",
-    "metallic",
-    "albedo",
-    "color"
-)
+from core import sql
 
 
 def get_projects():
-    pass
+    """
+    Return a list of (id, name) of projects
+    """
+    data = sql.list_projects()
+    names = [(d['id'], d['name']) for d in data]
+    return names
 
 
-def get_assets(project):
-    pass
+def get_groups(id_project):
+    """
+    Return a list of (id, name) of groups
+    """
+    data = sql.list_groups(id_project)
+    names = [(d['id'], d['name']) for d in data]
+    return names
+
+
+def get_assets(id_group):
+    """
+    Return a list of (id, name) of assets
+    """
+    data = sql.list_assets(id_group)
+    names = [(d['id'], d['name']) for d in data]
+    return names
 
 
 def check_texture_type():
--- ui/MainWindow.py
+++ ui/MainWindow.py
@@ -1,19 +1,74 @@
 __author__ = 'a.paoletti'
 
 from PySide import QtCore, QtGui, QtUiTools
-import sys
+
+from core import UploaderUtilities
 
 
 class UploadToolUI(object):
 
     def __init__(self):
-        self.ui = self.load_ui_widget("MainWindow.ui")
+        self.ui = self.load_ui_widget("ui/MainWindow.ui")
+
         self.setup_connections()
+
+        self.fill_projects()
 
     def setup_connections(self):
         self.ui.button_fbx.clicked.connect(self.get_files)
 
+        # connect projects
+        self.ui.comboBox_projects.currentIndexChanged[int].connect(self.fill_groups)
+
+        # connect groups
+        self.ui.comboBox_group.currentIndexChanged[int].connect(self.fill_assets)
+
+        # self.ui.button_upload.clicked.connect(self.test)
+
+    def test(self):
+        print self.ui.comboBox_projects.itemData(self.ui.comboBox_projects.currentIndex())
+
+    def fill_projects(self):
+        # fill projects combo box
+        for project in UploaderUtilities.get_projects():
+            id = project[0]
+            name = project[1]
+            self.ui.comboBox_projects.addItem(name, (id, name))
+
+    def fill_groups(self, index):
+        # clear combo box
+        self.ui.comboBox_group.clear()
+
+        data = self.ui.comboBox_projects.itemData(index)
+        id_project = None
+        if data:
+            id_project = data[0]
+
+        if id_project:
+            # fill group combo box
+            for group in UploaderUtilities.get_groups(id_project):
+                id = group[0]
+                name = group[1]
+                self.ui.comboBox_group.addItem(name, (id, name))
+
+    def fill_assets(self, index):
+        # clear combo box
+        self.ui.comboBox_asset.clear()
+
+        data = self.ui.comboBox_group.itemData(index)
+        id_group = None
+        if data:
+            id_group = data[0]
+
+        if id_group:
+            # fill assets combo box
+            for asset in UploaderUtilities.get_assets(id_group):
+                id = asset[0]
+                name = asset[1]
+                self.ui.comboBox_asset.addItem(name, (id, name))
+
     def get_files(self):
+        """ Open a file dialog in order to choose files to upload """
         f_dialog = QtGui.QFileDialog()
         f_dialog.setFileMode(QtGui.QFileDialog.ExistingFiles)
         f_dialog.setDirectory("C:\\Users\\a.paoletti\\Desktop\\STUFF")
@@ -38,12 +93,3 @@ class UploadToolUI(object):
 
     def show(self):
         self.ui.show()
-
-
-if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-
-    window = UploadToolUI()
-    window.show()
-
-    sys.exit(app.exec_())
