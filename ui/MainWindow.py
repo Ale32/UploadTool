a36971eecb29ea101261d2f143e9103cd0bef2a5
Alessio Paoletti <a.paoletti@rbw-cgi.it>
2017-03-31 18:17:57 +0200

    add clear button for the list widget and upload functions

--- ui/MainWindow.py
+++ ui/MainWindow.py
@@ -3,6 +3,7 @@ __author__ = 'a.paoletti'
 from PySide import QtCore, QtGui, QtUiTools
 
 from core import UploaderUtilities
+from core import Uploader
 
 
 class UploadToolUI(object):
@@ -17,16 +18,33 @@ class UploadToolUI(object):
     def setup_connections(self):
         self.ui.button_fbx.clicked.connect(self.get_files)
 
+        # clear button
+        self.ui.button_clearList.clicked.connect(self.clear_selection)
+
         # connect projects
         self.ui.comboBox_projects.currentIndexChanged[int].connect(self.fill_groups)
 
         # connect groups
         self.ui.comboBox_group.currentIndexChanged[int].connect(self.fill_assets)
 
-        # self.ui.button_upload.clicked.connect(self.test)
+        self.ui.button_upload.clicked.connect(self.upload)
 
-    def test(self):
-        print self.ui.comboBox_projects.itemData(self.ui.comboBox_projects.currentIndex())
+    def upload(self):
+        # get data selected
+        project = self.ui.comboBox_projects.itemData(self.ui.comboBox_projects.currentIndex())[1]
+        group = self.ui.comboBox_group.itemData(self.ui.comboBox_group.currentIndex())[1]
+        asset = self.ui.comboBox_asset.itemData(self.ui.comboBox_asset.currentIndex())[1]
+
+        # initialized uploader
+        uploader = Uploader.Uploader(project, group, asset)
+
+        selected_files = []
+        for index in xrange(self.ui.listWidget.count()):
+            selected_files.append(self.ui.listWidget.item(index).text())
+
+        # upload selected files
+        for f in selected_files:
+            uploader.upload(f)
 
     def fill_projects(self):
         # fill projects combo box
@@ -78,6 +96,9 @@ class UploadToolUI(object):
         if f_dialog.exec_():
             filenames = f_dialog.selectedFiles()
             self.ui.listWidget.addItems(filenames)
+
+    def clear_selection(self):
+        self.ui.listWidget.clear()
 
     @staticmethod
     def load_ui_widget(uifilename, parent=None):
