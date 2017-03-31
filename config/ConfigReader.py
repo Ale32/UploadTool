195ba7aeb990fa4d21c8624b57545b375fe7a1d4
Alessio Paoletti <a.paoletti@rbw-cgi.it>
2017-03-31 18:14:32 +0200

    add other formats and config reader functions

--- config/ConfigReader.py
+++ config/ConfigReader.py
@@ -4,6 +4,10 @@ import os
 config_data = None
 
 
+class ConfigException(Exception):
+    pass
+
+
 def read_config():
     global config_data
 
@@ -22,10 +26,41 @@ def read_config():
 def db_path():
     config_data = read_config()
 
-    if not config_data['paths']['database']:
-        return 'database.db'
+    database = config_data['paths']['database']
+    if not database:
+        raise ConfigException("Missing database path in config file!")
 
-    return config_data['paths']['database']
+    return database
+
+
+def server_name():
+    config_data = read_config()
+
+    server = config_data['paths']['server']
+    if not server:
+        raise ConfigException("Missing server name in config file!")
+
+    return server
+
+
+def share_name():
+    config_data = read_config()
+
+    share = config_data['paths']['share']
+    if not share:
+        raise ConfigException("Missing share name in config file!")
+
+    return share
+
+
+def content_name():
+    config_data = read_config()
+
+    content = config_data['paths']['content']
+    if not content:
+        raise ConfigException("Missing content name in config file!")
+
+    return content
 
 
 def accepted_format(filename, file_type):
@@ -56,3 +91,36 @@ def generate_texture_filter():
     config_data = read_config()
     file_formats = ['*.{}'.format(f.lower()) for f in config_data['file_formats']['texture']]
     return ' '.join(file_formats)
+
+
+def asset_prefix(asset_type):
+    config_data = read_config()
+
+    prefix = config_data['asset_prefix'][asset_type]
+    if not prefix:
+        raise ConfigException("Missing asset prefix in config file!")
+
+    return prefix
+
+
+def texture_suffix(filename):
+    config_data = read_config()
+
+    filename = os.path.splitext(filename)[0]
+    txt_part = filename.split("_")[-1]
+
+    for txt_type in config_data['texture_naming']:
+        if txt_part in config_data['texture_naming'][txt_type]['text']:
+            return config_data['texture_naming'][txt_type]['suffix']
+
+    return None
+
+
+def file_type_from_ext(ext):
+    config_data = read_config()
+
+    for file_type in config_data['file_formats']:
+        if ext.lower() in config_data['file_formats'][file_type]:
+            return file_type
+
+    raise ConfigException("File type not recognize!")
